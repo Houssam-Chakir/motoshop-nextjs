1b0b1defea63918602595405b33906bd342e4aa3
houssam chakir <thisischchannel@gmail.com>
2025-05-06 12:10:59 +0100

    Implementing Authentication UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/components/authentication/AuthProvider";
+import NavBar from "@/components/Navbar";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -26,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <AuthProvider>
-        <body>{children}</body>
+        <body>
+          <NavBar />
+          {children}
+        </body>
       </AuthProvider>
     </html>
   );
--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,3 +1,4 @@
+import { Profile, Session } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -16,14 +17,14 @@ export const authOptions = {
   ],
   callbacks: {
     // invoked on successful sign in
-    async signIn({ profile }) {
+    async signIn({ profile }: {profile: Profile}) {
       // 1 connect to database
       // 2 check if user exists
       // 3 if not, create new user
       // 4 Return true to allow sign in
     },
     // session callback function that modifies the session object
-    async session({ session }) {
+    async session({ session }: {session: Session}) {
       // 1 get user from database
       // 2 asign user id from session
       // 3 return the session
